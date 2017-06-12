55dd532d27f2df436ac7faf6ece21c88237ad103
Jose Fernandez <jfz@qlik.com>
2017-06-12 14:29:11 +0200

    fixing some stuff fixing some stuff

--- routes/main.js
+++ routes/main.js
@@ -1,65 +1,72 @@
-var express = require('express');
+var express = require( 'express' );
 var router = express.Router();
-var engine = require('../engine');
-var authenticate = require('../authenticate')
-var generateID = require('../generateSession')
-const config = require('../config');
-var spot = require('../spotify');
-const mcache = require('memory-cache');
+var engine = require( '../engine' );
+var authenticate = require( '../authenticate' );
+var generateID = require( '../generateSession' );
+const config = require( '../config' );
+var spotify = require( '../spotify' );
+const mcache = require( 'memory-cache' );
 
-function log(m, req) {
-    console.log(req.connection.remoteAddress, m);
+function log ( m, req ) {
+	console.log( req.connection.remoteAddress, m );
 }
-router.get('/session/*', function (req, res, next) {
-	var sessionId=req.path.substr(req.path.indexOf('/session/')+9,req.path.length);
-	var userId=mcache.get(sessionId);
-	authenticate({id: userId, directory: config.directory}, sessionId).then((d)=> {
-		res.status(200).send(d);
-	})
-})
-router.post('/', function (req, res, next) {
-    if (req.body.template && req.session.token) {
-			spot.getBasic(req.session.token).then((data)=>{
-				var uid = req.body.userid;
+router.get( '/session/*', function ( req, res, next ) {
+	var sessionId = req.path.substr( req.path.indexOf( '/session/' ) + 9, req.path.length );
+	var userId = mcache.get( sessionId );
+	authenticate( {id: userId, directory: config.directory}, sessionId ).then( function ( d ) {
+		res.status( 200 ).send( d );
+	} );
+} );
+
+router.post( '/', function ( req, res, next ) {
+	if ( req.body.code ) {
+		spotify.prepareData( req.body.code ).then( function ( data ) {
 				var script = data.script;
-				if(req.session.sessionID){
-					engine.createSessionAppFromScratch({
-							cookie: config.cookieName + "=" + req.session.sessionID + '; Path=/; HttpOnly; Secure',
-							template:config.emptyAppID,
-							script: script,
-							appName:data.name
-						}).then((result)=> {
-						req.session.currentApp=result;
-						res.status(200).send(result)
-					}).catch((err)=> {
-							console.log(err)
-							res.status(500).send(err)
+				if ( req.session.id ) {
+					engine.createSessionAppFromScratch( {
+						cookie: config.cookieName + "=" + req.session.id + '; Path=/; HttpOnly; Secure',
+						template: config.emptyAppID,
+						script: script,
+						appName: data.name
+					} ).then( function ( result ) {
+						req.session.currentApp = result;
+						res.status( 200 ).send( result );
+					} ).catch( function ( err ) {
+							console.log( err );
+							res.status( 500 ).send( err )
 						}
 					)
 				}
-			},(e)=>{
-				console.log(e);
-				res.status(200).send({error:"Error in Spotify communication",stack:e.error})
-			}).catch((e)=>{res.status(500).send(err)});
-    }
-	else if (req.body.auth) {
-		req.session.token=req.body.auth;
-		spot.getUser(req.session.token).then((data)=>{
-			console.log("Logged:",data);
-			req.session.sessionID=generateID();
-			mcache.put(req.session.sessionID,data);
-			req.session.userId=data;
-			res.cookie(config.cookieName, req.session.sessionID, {expires: 0, httpOnly: true});
-			res.status(200).send(req.session.token);
-		},(e)=>{
-			//res.redirect(307,'/login.html');
-			res.status(200).send({redirect:'/',error:"Error getting user info",stack:e})
-		})
+			},
+			function ( e ) {
+				console.log( e );
+				res.status( 200 ).send( {error: "Error in Spotify communication", stack: e.error} )
+			}
+		).catch( function ( e ) {
+			res.status( 500 ).send( err )
+		} );
 	}
-    else {
-        log("NADA", req);
-        res.status(200).send({})
-    }
-});
+	// else if ( req.body.code ) {
+	// 	//One we have the code we have to exchange it with a token.
+	// 	var code = req.body.code;
+	//
+	// 	spotify.getUser( code ).then( function ( data ) {
+	// 			console.log( "Logged:", data );
+	// 			req.session.sessionID = generateID();
+	// 			mcache.put( req.session.sessionID, data );
+	// 			req.session.userId = data;
+	// 			res.cookie( config.cookieName, req.session.sessionID, {expires: 0, httpOnly: true} );
+	// 			res.status( 200 ).send( req.session.token );
+	// 		},
+	// 		function ( e ) {
+	// 			//res.redirect(307,'/login.html');
+	// 			res.status( 200 ).send( {redirect: '/', error: "Error getting user info", stack: e} )
+	// 		} );
+	// }
+	else {
+		log( "NADA", req );
+		res.status( 200 ).send( { test: "hi"} )
+	}
+} );
 
 module.exports = router;
